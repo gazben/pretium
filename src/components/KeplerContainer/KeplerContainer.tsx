4d44446d465a52e2d0bde700e22c0a705d165212
Peter Balogh <pterblgh@gmail.com>
2019-10-26 13:31:40 +0200

    feat(kepler): display full-screen kepler component

--- src/components/KeplerContainer/KeplerContainer.tsx
+++ src/components/KeplerContainer/KeplerContainer.tsx
@@ -1,5 +1,23 @@
+import KeplerGl from 'kepler.gl';
 import React from 'react';
+import { ReactReduxContext } from 'react-redux';
+
+const token =
+  'pk.eyJ1IjoicHRlcmJsZ2giLCJhIjoiY2syN2dyODRyMHY1eTNia3h6eWQydTA2ZCJ9.JFwFiFT4YnMDc8hCjMicEg';
 
 export const KeplerContainer = () => {
-  return <p>Stuff will be rendered here</p>;
+  return (
+    <ReactReduxContext.Consumer>
+      {({ store }) => {
+        return (
+          <KeplerGl
+            width={window.outerWidth}
+            height={window.outerHeight}
+            mapboxApiAccessToken={token}
+            store={store}
+          />
+        );
+      }}
+    </ReactReduxContext.Consumer>
+  );
 };
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,3 @@
 declare module 'kepler.gl/reducers';
 declare module 'kepler.gl/middleware';
+declare module 'kepler.gl';
