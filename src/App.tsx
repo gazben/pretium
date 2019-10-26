e6a7d8317cf607ac7951e4515d927a2eb8682567
Peter Balogh <pterblgh@gmail.com>
2019-10-26 12:26:32 +0200

    feat(KeplerContainer): add skeleton for kepler

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Landing } from './components';
+import { KeplerContainer, Landing } from './components';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/visualize">
+          <KeplerContainer />
+        </Route>
         <Route path="/">
           <Landing />
         </Route>
       </Switch>
     </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ src/components/KeplerContainer/KeplerContainer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const KeplerContainer = () => {
+  return <p>Stuff will be rendered here</p>;
+};
